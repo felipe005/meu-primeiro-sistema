598a22e7a39ac4b0e9072ecd12579ceba5cba7a3
Felipe <felipemarques27052003@gmail.com>
2026-02-20 12:45:58 -0300

    corrigindo porta para Azure

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get("/salvar", (req, res) => {
 
 // Iniciar servidor
 app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+  console.log("Servidor rodando na porta 3000");
 });
